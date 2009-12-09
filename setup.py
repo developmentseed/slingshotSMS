03ecd12b2a1e193578a4c03bda05f72558110afd
Tom MacWright <macwright@gmail.com>
2009-12-09 17:02:54 -0500

    Setup.py updates to work with other systems cleanly instead of as hacks

--- setup.py
+++ setup.py
@@ -1,4 +1,33 @@
-from distutils.core import setup
-import py2exe
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
-setup(console=['slingshotsms.py'], data_files=['README'])
+import ez_setup
+ez_setup.use_setuptools()
+import sys
+from setuptools import setup
+
+# Support for building on mac & linux, and installation
+# on Unix
+if sys.platform == 'darwin':
+    extra_options = dict(
+        setup_requires=['py2app'],
+        app=['slingshotsms.py'],
+        )
+elif sys.platform  == 'win32':
+     extra_options = dict(
+         setup_requires=['py2exe'],
+         app=[mainscript],
+         )
+else:
+     extra_options = dict(
+         # Normally unix-like platforms will use "setup.py install"
+         # and install the main script as such
+         scripts=[mainscript],
+         )
+    
+
+setup(
+    name="SlingshotSMS",
+    data_files=['README.rst'],
+    **extra_options
+    )
