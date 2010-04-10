7f8c3231eaa4cb4dfc75b152d8861779903cfae9
Tom MacWright <macwright@gmail.com>
2010-04-10 13:45:35 -0400

    Adding better styling and more of a js stub

--- web/slingshot.js
+++ web/slingshot.js
@@ -3,3 +3,18 @@
  * (c) Tom MacWright 2010
  */
 
+var SlingshotSMS = function(opts) {
+	this.init(opts);
+}
+
+SlingshotSMS.prototype = {
+  init: function() {
+    console.log('not implemented');
+  },
+  send: function() {
+    console.log('not implemented');
+  },
+  receive: function() {
+    console.log('not implemented');
+  }
+};
