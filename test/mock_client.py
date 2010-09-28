a572558abe08001a6a2e8401f10ef622d00fd8bf
Tom MacWright <macwright@gmail.com>
2010-09-28 15:19:41 -0400

    Cleaning up mock client

--- test/mock_client.py
+++ test/mock_client.py
@@ -1,18 +1,15 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
 from optparse import OptionParser
 from sqlobject import SQLObject, IntCol, StringCol, BLOBCol
 from sqlobject.sqlite.sqliteconnection import SQLiteConnection
 
-class ContactData(SQLObject):
-    _connection = SQLiteConnection('slingshotsms.db')
-    # in reference to
-    # http://en.wikipedia.org/wiki/VCard
-    TEL = StringCol()
-    UID = StringCol()
-    PHOTO = BLOBCol()
-    N = StringCol()
-    FN = StringCol()
-    # contains all data as serialized vc, including the above columns
-    data = BLOBCol()
+'''
+
+  mock_client: this is a script that helps testing of SlingshotSMS
+  without having a real modem or other infrastructre
+
+'''
 
 class MessageData(SQLObject):
     _connection = SQLiteConnection('slingshotsms.db')
@@ -29,12 +26,14 @@ class OutMessageData(SQLObject):
 
 
 parser = OptionParser()
+
 parser.add_option("-t", "--text", dest="text",
                   help="Your Message")
 parser.add_option("-s", "--sender", dest="sender",
                   help="Your Message")
 parser.add_option("-n", "--timestamp", dest="sent",
                   help="Your Message")
+
 (options, args) = parser.parse_args()
 
 MessageData(text = options.text, sender = options.sender, sent = int(options.sent))
