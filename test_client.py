9a4248d3ca2255516d708cd81a6efa379a542a5c
Tom MacWright <macwright@gmail.com>
2010-09-29 23:19:50 -0400

    Sweet new testing machinery

--- test_client.py
+++ test_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
-import sys, time
+import sys, time, readline, multiprocessing
 from optparse import OptionParser
 from sqlobject import SQLObject, IntCol, StringCol, BLOBCol
 from sqlobject.sqlite.sqliteconnection import SQLiteConnection
@@ -19,10 +19,41 @@ class MessageData(SQLObject):
     sender = StringCol()
     text = StringCol()
 
+
+class OutMessageData(SQLObject):
+    _connection = SQLiteConnection('slingshotsms.db')
+    number = StringCol()
+    text = StringCol()
+
+def sent_watcher():
+    while True:
+        messages = OutMessageData.select()
+        for message in messages:
+            print "sent: \n%s\n to %s" % (message.text, message.number)
+            message.destroySelf()
+        time.sleep(5)
+
 if __name__ == '__main__':
     o = OptionParser(usage='Usage: %prog 19777777 "A message"')
+    o.add_option('--i', '--interactive', action='store_true', dest='interactive')
     (opts, args) = o.parse_args()
-    if len(args) < 2:
+    if opts.interactive:
+        print "Interactive mode"
+        sender = raw_input('[what number should messages originate from?]: ')
+
+        watcher = multiprocessing.Process(target=sent_watcher)
+        watcher.start()
+
+        input = True
+        while input:
+            input = raw_input('> ')
+            if input == 'exit':
+                watcher.join()
+                sys.exit(0)
+            MessageData(sender=sender, text=input, sent=int(time.time()))
+            print "sent message"
+    elif len(args) < 2:
         o.print_usage()
         sys.exit(1)
-    MessageData(sender=args[0], text=args[1], sent=int(time.time()))
+    else:
+        MessageData(sender=args[0], text=args[1], sent=int(time.time()))
